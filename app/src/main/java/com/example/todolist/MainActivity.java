834c9809664d1715bce70c7c944d7a07474242e2
Belindaanindya <belindaanindya@gmail.com>
2020-10-14 14:55:53 +0700

    Layout Intregation

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
+    Button addButton;
     String[] listItem;
 
     @Override
@@ -20,17 +23,29 @@ public class MainActivity extends AppCompatActivity {
 
         listView = (ListView)findViewById(R.id.to_do_list);
         listItem = getResources().getStringArray(R.array.to_do_array);
+        addButton = (Button) findViewById(R.id.add_btn);
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItem);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                // TODO Auto-generated method stub
                 String value = adapter.getItem(position);
                 Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
 
             }
         });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAddActivity();
+            }
+        });
+    }
+
+    public void openAddActivity() {
+        Intent intent = new Intent(this, AddActivity.class);
+        startActivity(intent);
     }
 }
